1b8b78c9c1d201e8c8210bdd026a418877f8bee7
Meriem4d <44900125+Meriem4d@users.noreply.github.com>
2019-05-17 13:53:31 +0200

    Update 'projectName' in siteConfig.js
    ie. fix build path

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -32,7 +32,7 @@ const siteConfig = {
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'docs',
+  projectName: '4d',
   organizationName: '4D',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
