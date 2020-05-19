3eb73dc2a7b78e9cd99d710ed417d3ee375b9ccf
4DBuildMgr <victor@imac-steininger.private.4d.fr>
2020-05-19 09:29:36 +0200

    Update conf for alpha branch

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -26,7 +26,7 @@ const siteConfig = {
   url: 'https://4d.github.io' /* your website url */,
   //baseUrl: '/doc/' /* base url for your project */,
   //baseUrl: '/docs/' /* base url for your project */,
-  baseUrl: '/docs/' /* base url for your project */,
+  baseUrl: '/docs/alpha/' /* base url for your project */,
 // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
@@ -45,9 +45,11 @@ const siteConfig = {
 
     //{doc: 'Concepts/about', label: 'Docs'},
     //{doc: 'REST/gettingStarted', label: 'REST'},
-
-	//{href: 'https://developer.4d.com/', label:'Developer Center'},
-	//{href: 'http://kb.4d.com/', label: 'knowledgebase'},
+    {href: 'https://developer.4d.com/docs', label:'v18 R3 BETA'},
+    {href: 'https://developer.4d.com/docs/Rx', label:'v18 R2'},
+    {href: 'https://developer.4d.com/docs/18', label:'v18'},
+    //{href: 'https://developer.4d.com/', label:'Developer Center'},
+    //{href: 'http://kb.4d.com/', label: 'knowledgebase'},
     //{page: 'help', label: 'Help'},
     //{blog: false, label: 'Blog'},
   ],
