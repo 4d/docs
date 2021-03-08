1767b10a86e9d06eb1b54144cd1cf7c40a3d044b
F4r3n <guillaume_k@hotmail.fr>
2021-03-08 17:56:51 +0100

    search

---
+++ website/pages/en/searchAPI.js
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2017-present, Facebook, Inc.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+const React = require("react");
+
+class RedirectAPI extends React.Component {
+  render() {
+    const l = JSON.stringify(this.props.config.headerLinks)
+    const language = this.props.language
+    return (
+      <html lang="en">
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              const obj = JSON.parse('${l}')
+              const url = new URL(window.location.href);
+              const versionWanted = url.searchParams.get("v");
+              const classWanted = url.searchParams.get("class");
+              const member = url.searchParams.get("member");
+
+              let finalUrl = ""
+
+              //Match version
+              for(let i = 0; i < obj.length; i++) {
+                const version = obj[i].version
+                if(version === versionWanted) {
+                  finalUrl = obj[i].href
+                }
+              }
+
+              if(finalUrl === "") {
+                finalUrl = obj[0].href
+              }
+              finalUrl += "/${language}" + "/API/" + classWanted + "Class" + "#" + member
+              window.location.href = finalUrl
+                `,
+            }}
+          />
+
+      </html>
+    );
+  }
+}
+
+module.exports = RedirectAPI;
