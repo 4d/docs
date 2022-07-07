02dfe7f0bf37b68f900e1a0866ff962ea4b2db2e
Tony Briget <tony.briget@4d.com>
2022-07-07 17:56:14 +0200

    add footer, fix navbar alignment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -62,11 +62,89 @@ module.exports = {
     },
     "image": "../assets/img/en/logohome.png",
     "footer": {
-      "links": [],
+      style: 'dark',
+      links: [
+        {
+          title: 'Community',
+          items: [
+            {
+              label: '4D Developer Center',
+              to: 'https://developer.4d.com',
+            },
+            {
+              label: '4D Blog',
+              to: 'https://blog.4d.com',
+            },
+            {
+              label: '4D Forum',
+              to: 'https://discuss.4d.com',
+            },
+            {
+              label: 'Facebook',
+              to: 'https://facebook.com/4Dsoftware',
+            },
+            {
+              label: 'Twitter',
+              to: 'https://twitter.com/4Dsoftware',
+            },
+            {
+              label: 'Youtube',
+              to: 'https://youtube.com/user/4Dsoftware',
+            },
+            {
+              label: 'Github',
+              to: 'https://github.com/4D',
+            },
+          ],
+        },
+        {
+          title: 'Support',
+          items: [
+            {
+              label: '4D Doc Center (Where It All Began)',
+              to: 'https://doc.4d.com',
+            },
+            {
+              label: 'Knowledgebase',
+              to: 'kd.4d.com',
+            },
+            {
+              label: 'Downloads',
+              href: 'https://github.com/facebook/docusaurus',
+            },
+            {
+              label: 'Resources',
+              href: 'https://us.4d.com/resources',
+            },
+            {
+              label: 'Get Support',
+              href: 'https://us.4d.com/4d-technical-support',
+            }
+          ],
+        },
+        {
+          title: 'Company',
+          items: [
+            {
+              label: 'About 4D',
+              href: 'https://us.4d.com/about-us',
+            },
+            {
+              label: 'Contact us',
+              href: 'https://us.4d.com/contact-us',
+            },
+            {
+              label: '4D around the world',
+              href: 'https://us.4d.com/4d-around-the-world',
+            },
+            {
+              label: 'Careers',
+              href: 'https://us.4d.com/Careers',
+            },
+          ],
+        },
+      ],
       "copyright": "© 2022 4D SAS - All rights reserved",
-      "logo": {
-        "src": "../assets/img/en/logohome.png"
-      }
     },
   }
 }
