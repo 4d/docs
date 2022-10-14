6522695ec0dfca860c47e2bf3288f776e583bca5
arnaud-4d <arnaud.schmitt@4d.com>
2022-10-14 10:43:58 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -105,10 +105,10 @@ module.exports = {
 		apiKey: '5f22ebbb9382abafeadc3e86ca47d4af',
 		appId: 'OJ04C0M3CU',
       indexName: '4d',
-	  //algoliaOptions: {
-      //facetFilters: ['language:LANGUAGE', 'version:VERSION'],
-    //},
-      contextualSearch: true
+	searchParameters: {
+	      facetFilters: ['language:LANGUAGE', 'version:VERSION'],
+    	},
+      contextualSearch: false,
     },
     prism: {
       theme: require("./src/theme/highlight/themes/palenight"),
