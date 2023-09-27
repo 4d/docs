554cbc5e2a331bdfa467642739f46c49c6b8b9ce
f4r3n <guillaume_k@hotmail.fr>
2023-09-27 21:54:13 +0200

    wip

--- prebuild.js
+++ prebuild.js
@@ -4,21 +4,24 @@ const util = require('util');
 const path = require("path")
 
 let args = process.argv.slice(2);
-const mainFolder = args[0]
+const mainFolder = "/"
 const readdir = util.promisify(fs.readdir);
 const readFile = util.promisify(fs.readFile);
 
-
-function collect(mainFolder) {
+function collect(mainFolder, inRegex) {
     return new Promise( async (resolve, reject)=>{
+
         if(!fs.existsSync(mainFolder))
         {
             resolve({})
             return;
         }
+
         let data = {}
         let promises = []
         let files = await readdir(mainFolder)
+        const regex = inRegex
+
 
         files.forEach((file)=> {
             if(file.endsWith(".md")) {
@@ -26,7 +29,6 @@ function collect(mainFolder) {
                 const p = new Promise((resolve, reject)=> {
                     let localData = {}
                     content.then((content)=> {
-                        const regex = /<!--\s*REF\s+#_command_.([0-9a-zA-Z\s-_]+)/g
                         const matches = [...content.matchAll(regex)].map(m=>m[1]).filter((value, index, self)=>self.indexOf(value) == index);
                         matches.forEach((value)=> {
                             localData[value.toLocaleLowerCase()] = path.join(mainFolder, file.split('.').slice(0, -1).join('.')).replace(/\\/g, '/');
@@ -41,9 +43,8 @@ function collect(mainFolder) {
             {
                 if(fs.lstatSync(mainFolder + file).isDirectory())
                 {
-                    //listFolders.push(mainFolder + file + '/')
                     const localPath = path.join(mainFolder,file) + path.sep
-                    promises.push(collect(localPath))
+                    promises.push(collect(localPath, inRegex))
                 }
             }
         })
@@ -65,7 +66,11 @@ let listVersions = []
 for(v of versions)
 {
     const currentV = v.replace(/\\/g, '/')
-    listVersions.push(new Promise((resolve) => collect(path.join(v, mainFolder)).then(data =>resolve({version:currentV, data:data}))))
+    listVersions.push(new Promise((resolve) => collect(path.join(v, mainFolder), /<!--\s*REF\s+#_command_.([0-9a-zA-Z\s\-_]+)/g)
+    .then(data =>resolve({version:currentV, data:data, name:"command"}))))
+
+    listVersions.push(new Promise((resolve) => collect(path.join(v, mainFolder), /<!--\s*REF\s+#([0-9a-zA-Z\s\-_]+)(?<!_command_)\.[0-9a-zA-Z\s\-_]+.Syntax/g)
+    .then(data =>resolve({version:currentV, data:data, name:"class"}))))
 }
 
 Promise.all(listVersions).then((localData)=> {
@@ -73,6 +78,7 @@ Promise.all(listVersions).then((localData)=> {
     let commandList = {}
     for(l of localData)
     {
+        const name = l.name
         const data = l.data;
         const version = l.version
         if(Object.keys(data).length !== 0) {
@@ -81,8 +87,17 @@ Promise.all(listVersions).then((localData)=> {
                 data[key] = content.replace(version + "/", "");
               });
         }
-        const cleanVersion=version.replace("versioned_docs/version-", "").replace("docs", "")
-        commandList[cleanVersion.replace("-","")] = data
+        const cleanVersion=version.replace("versioned_docs/version-", "").replace("docs", "").replace("-","")
+        if(!commandList.hasOwnProperty(cleanVersion)) {
+            commandList[cleanVersion] = {}
+        }
+        
+        let listForVersion = commandList[cleanVersion]
+        if(!listForVersion.hasOwnProperty(name))
+        {
+            listForVersion[name] = {}
+        }
+        listForVersion[name] = data
     }
     fs.writeFileSync("commandList.json", JSON.stringify(commandList))
 
