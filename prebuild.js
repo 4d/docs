e64949210de300bee67dd9aebde3a45c2dfdf29c
Guillaume.kotulski <guillaume.kotulski@4d.com>
2023-09-28 16:57:48 +0200

    prebuild use preprocessing

--- prebuild.js
+++ prebuild.js
@@ -1,76 +1,27 @@
 
 const fs = require('fs');
-const util = require('util');
 const path = require("path")
+const preprocess = require("@4dsas/doc_preprocessing/lib/preprocessor")
+const settings = require("@4dsas/doc_preprocessing/lib/settings")
 
-let args = process.argv.slice(2);
 const mainFolder = "/"
-const readdir = util.promisify(fs.readdir);
-const readFile = util.promisify(fs.readFile);
 
-function collect(mainFolder, inRegex) {
-    return new Promise( async (resolve, reject)=>{
-
-        if(!fs.existsSync(mainFolder))
-        {
-            resolve({})
-            return;
-        }
-
-        let data = {}
-        let promises = []
-        let files = await readdir(mainFolder)
-        const regex = inRegex
-
-
-        files.forEach((file)=> {
-            if(file.endsWith(".md")) {
-                const content = readFile(mainFolder + file,{encoding:"utf8"})
-                const p = new Promise((resolve, reject)=> {
-                    let localData = {}
-                    content.then((content)=> {
-                        const matches = [...content.matchAll(regex)].map(m=>m[1]).filter((value, index, self)=>self.indexOf(value) == index);
-                        matches.forEach((value)=> {
-                            localData[value.toLocaleLowerCase()] = path.join(mainFolder, file.split('.').slice(0, -1).join('.')).replace(/\\/g, '/');
-                        })
-                        resolve(localData)
-                    })
-                })
-                promises.push(p)
-
-            }
-            else
-            {
-                if(fs.lstatSync(mainFolder + file).isDirectory())
-                {
-                    const localPath = path.join(mainFolder,file) + path.sep
-                    promises.push(collect(localPath, inRegex))
-                }
-            }
-        })
-
-        Promise.all(promises).then((localData)=> {
-            for(const d of localData)
-            {
-                data = Object.assign(data, d)
-            }
-            resolve(data)
-        })
-
-    });
-}
 
 let versions = JSON.parse(fs.readFileSync("versions.json")).map(v=>path.join("versioned_docs","version-" +v))
 versions.push("docs")
 let listVersions = []
 for(v of versions)
 {
-    const currentV = v.replace(/\\/g, '/')
-    listVersions.push(new Promise((resolve) => collect(path.join(v, mainFolder), /<!--\s*REF\s+#_command_.([0-9a-zA-Z\s\-_]+)/g)
-    .then(data =>resolve({version:currentV, data:data, name:"command"}))))
+    const info = new settings.Settings()
+    info.set(settings.SETTINGS_KEY.PATH, path.join(v, mainFolder))
+    info.set(settings.SETTINGS_KEY.CONFIG, "preprocessing.conf")
+    info.set(settings.SETTINGS_KEY.EXCLUDE_LIST, ["ViewPro"])
 
-    listVersions.push(new Promise((resolve) => collect(path.join(v, mainFolder), /<!--\s*REF\s+#([0-9a-zA-Z\s\-_]+)(?<!_command_)\.[0-9a-zA-Z\s\-_]+.Syntax/g)
-    .then(data =>resolve({version:currentV, data:data, name:"class"}))))
+    const currentV = v.replace(/\\/g, '/')
+
+    listVersions.push(new Promise((resolve) => {let processor = new preprocess.Preprocessing(info); processor.collect()
+                                    .then(() =>resolve({version:currentV, data:processor.formatIndex()}))}))
+
 }
 
 Promise.all(listVersions).then((localData)=> {
@@ -78,27 +29,53 @@ Promise.all(listVersions).then((localData)=> {
     let commandList = {}
     for(l of localData)
     {
-        const name = l.name
         const data = l.data;
         const version = l.version
+        const cleanVersion=version.replace("versioned_docs/version-", "").replace("docs", "").replace("-","")
+
+        let commands = {}
+        
+        function cleanPath(inPath) {
+            return inPath.replace(/\\/g, '/').replace("versioned_docs/version-", "").replace("docs", "").replace("-","").replace(cleanVersion, "").replace(".md","").slice(1)
+        }
+
         if(Object.keys(data).length !== 0) {
             Object.keys(data).forEach(function(key, index) {
-                const content = data[key]
-                data[key] = content.replace(version + "/", "");
-              });
+                const split = key.split('.')
+                if(split[0] === "_command_") {
+                    commands[split[1].toLowerCase()] = cleanPath(data[key]);
+                }
+            });
         }
-        const cleanVersion=version.replace("versioned_docs/version-", "").replace("docs", "").replace("-","")
+
+        let classes = {}
+        if(Object.keys(data).length !== 0) {
+            Object.keys(data).forEach(function(key, index) {
+                const split = key.split('.')
+                if(split[0] !== "_command_" && split.slice(-1) == "Syntax" && split[0] !== "4D") {
+                    classes[split[0].toLocaleLowerCase()] = cleanPath(data[key]);
+                }
+            });
+        }
+
         if(!commandList.hasOwnProperty(cleanVersion)) {
             commandList[cleanVersion] = {}
         }
         
         let listForVersion = commandList[cleanVersion]
-        if(!listForVersion.hasOwnProperty(name))
+        if(!listForVersion.hasOwnProperty("commands"))
         {
-            listForVersion[name] = {}
+            listForVersion["commands"] = {}
         }
-        listForVersion[name] = data
+        listForVersion["commands"] = commands
+
+        if(!listForVersion.hasOwnProperty("classes"))
+        {
+            listForVersion["classes"] = {}
+        }
+        listForVersion["classes"] = classes
     }
-    fs.writeFileSync("commandList.json", JSON.stringify(commandList))
+    fs.writeFileSync("commandList.json", JSON.stringify(commandList, null, 2))
 
 })
+
