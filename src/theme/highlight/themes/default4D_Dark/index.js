bc4f6ecdc6e88b53d85ea2a1e94a936bfb83853e
Jim <jim_kerver@live.nl>
2024-09-02 14:37:43 -0600

    Add default 4D syntax colors as new themes

---
+++ src/theme/highlight/themes/default4D_Dark/index.js
@@ -0,0 +1,84 @@
+// Converted automatically using ./tools/themeFromVsCode
+var theme = {
+  plain: {
+    color: "#ffffff",
+    backgroundColor: "#292d3e"
+  },
+  styles: [{
+    types: ["comment"],
+    style: {
+      color: "rgb(143, 143, 143)",
+    }
+  }, {
+    types: ["string", "inserted"],
+    style: {
+      color: "rgb(255, 255, 255)"
+    }
+  }, {
+    types: ["number"],
+    style: {
+      color: "rgb(255, 255, 255)"
+    }
+  }, {
+    types: ["builtin", "char", "constant", "function"],
+    style: {
+      color: "rgb(89, 187, 0)",
+      fontWeight: "bold",
+    }
+  }, {
+    types: ["punctuation", "selector"],
+    style: {
+      color: "rgb(255, 255, 255)"
+    }
+  }, {
+    types: ["variable"],
+    style: {
+      color: "rgb(24, 179, 241)"
+    }
+  }, {
+    types: ["class-name", "attr-name"],
+    style: {
+      color: "rgb(255, 203, 107)"
+    }
+  }, {
+    types: ["tag", "deleted"],
+    style: {
+      color: "rgb(255, 85, 114)"
+    }
+  }, {
+    types: ["operator"],
+    style: {
+      color: "rgb(255, 255, 255)"
+    }
+  }, {
+    types: ["boolean"],
+    style: {
+      color: "rgb(089, 187, 0)",
+      fontWeight: "bold",
+    }
+  }, {
+    types: ["keyword"],
+    style: {
+      color: "rgb(0, 142, 0)",
+      fontWeight: "bold",
+    }
+  }, {
+    types: ["doctype"],
+    style: {
+      color: "rgb(199, 146, 234)",
+      fontStyle: "italic"
+    }
+  }, {
+    types: ["namespace"],
+    style: {
+      color: "rgb(178, 204, 214)"
+    }
+  }, {
+    types: ["url"],
+    style: {
+      color: "rgb(221, 221, 221)"
+    }
+  }]
+};
+
+export default theme;
---
+++ src/theme/highlight/themes/default4D_Light/index.cjs.js
@@ -0,0 +1,128 @@
+'use strict';
+
+// Converted automatically using ./tools/themeFromVsCode
+var theme = {
+  plain: {
+    color: "#000000",
+    backgroundColor: "#F6F8FA"
+  },
+  styles: [{
+    types: ["comment"],
+    style: {
+      color: "rgb(83, 83, 83)",
+    }
+  }, {
+    types: ["string", "inserted"],
+    style: {
+      color: "rgb(0, 0, 0)"
+    }
+  }, {
+    types: ["number"],
+    style: {
+      color: "rgb(0, 0, 0)"
+    }
+  },
+  {
+    types: ["char", "constant"],
+    style: {
+      color: "rgb(0, 0, 0)",
+    }
+  },
+  {
+    types: ["memberFunction"],
+    style: {
+      color: "rgb(95, 142, 94)",
+      fontStyle: "italic",
+    }
+  },
+  {
+    types: ["function"],
+    style: {
+      color: "rgb(6, 140, 0)",
+      fontWeight: "bold",
+    }
+  },
+  {
+    types: ["builtin", "command"],
+    style: {
+      color: "rgb(6, 140, 0)",
+      fontWeight: "bold"
+    }
+  }, 
+  {
+    types: ["property"],
+    style: {
+      color: "rgb(160, 128, 107)",
+    }
+  },
+  {
+    types: ["variable"],
+    style: {
+      color: "rgb(0, 49, 225)"
+    }
+  },
+  {
+    types: ["interprocess-variable"],
+    style: {
+      color: "rgb(255, 0, 136)"
+    }
+  }, 
+  {
+    types: ["parameters"],
+    style: {
+      color: "rgb(12, 112, 255)"
+    }
+  }, 
+  {
+    types: ["punctuation", "selector"],
+    style: {
+      color: "rgb(0, 0, 0)"
+    }
+  }, {
+    types: ["class-name", "attr-name"],
+    style: {
+      color: "rgb(255, 203, 107)"
+    }
+  }, {
+    types: ["tag", "deleted"],
+    style: {
+      color: "rgb(255, 85, 114)"
+    }
+  }, {
+    types: ["operator"],
+    style: {
+      color: "rgb(0, 0, 0)"
+    }
+  }, {
+    types: ["boolean"],
+    style: {
+      color: "rgb(6, 140, 0)",
+      fontWeight: "bold",
+    }
+  }, {
+    types: ["keyword"],
+    style: {
+      color: "rgb(3, 77, 0)",
+      fontWeight: "bold"
+    }
+  }, {
+    types: ["doctype"],
+    style: {
+      color: "rgb(199, 146, 234)",
+      fontStyle: "italic"
+    }
+  }, {
+    types: ["namespace"],
+    style: {
+      color: "rgb(178, 204, 214)",
+      fontWeight: "bold"
+    }
+  }, {
+    types: ["url"],
+    style: {
+      color: "rgb(221, 221, 221)"
+    }
+  }]
+};
+
+module.exports = theme;
