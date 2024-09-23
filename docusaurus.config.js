b9e5ebe73e8aa0175216783be44a9f84caf54960
Guillaume Kotulski <guillaume.kotulski@4d.com>
2024-09-23 13:26:38 +0200

    speed up build

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -69,6 +69,28 @@ module.exports = {
       },
     ],
   ],
+  webpack: {
+    jsLoader: (isServer) => ({
+      loader: require.resolve("swc-loader"),
+      options: {
+        jsc: {
+          parser: {
+            syntax: "typescript",
+            tsx: true,
+          },
+          target: "es2019",
+          transform: {
+            react: {
+              runtime: "automatic",
+            },
+          },
+        },
+        module: {
+          type: isServer ? "commonjs" : "es6",
+        },
+      },
+    }),
+  },
   i18n: {
     defaultLocale: "en",
     locales: ["en", "fr", "es", "ja", "pt"],
