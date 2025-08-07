49a79882c9813e2759ed1fab502790fc49e4d8f9
Guillaume.kotulski <guillaume.kotulski@4d.com>
2025-08-07 16:32:12 +0200

    fix copy

--- tools/copy_syntax.js
+++ tools/copy_syntax.js
@@ -3,8 +3,8 @@ const path = require('path');
 let exclude = ["cs", "de"];
 
 let default_file = "./syntax/en.lproj/syntaxEN.json";
-let dest = "/Users/gkotulski/Git/4eDimension/20R7/4D/Resources/";
-for(let language of ["en", "es", "pt", "es", "cs", "ja", "de"]) {
+let dest = "/Users/gkotulski/Git/4eDimension/20R10/4D/Resources/";
+for(let language of ["en", "es", "pt", "es", "cs", "ja", "de", "fr"]) {
     let file = `./syntax/${language}.lproj/syntax${language.toUpperCase()}.json`;
     if(exclude.includes(language)) {
         console.log(`Takes english ${language}`);
