2ec351ded709ac7a69eb71ec2226f1bdfc86399f
arnaud-4d <arnaud.schmitt@4d.com>
2025-11-17 19:06:45 +0100

    added new FORM Windows theme command

--- sidebars.js
+++ sidebars.js
@@ -604,7 +604,8 @@ module.exports =
                 "commands-legacy/form-set-output",
                 "commands-legacy/form-set-size",
                 "commands-legacy/form-set-vertical-resizing",
-                "commands-legacy/form-unload"
+                "commands-legacy/form-unload",
+                "commands/form-windows-theme"
               ]
             },
             {
