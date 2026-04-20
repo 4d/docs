526191918b3c27a2c0ba83518723d39b2a56ffd4
arnaud-4d <arnaud.schmitt@4d.com>
2026-04-20 16:43:50 +0200

    no beta

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -238,7 +238,7 @@ sidebarItemsGenerator: async function (args) {
 },
           versions: {
               '21-R2': {
-              label: '21 R2 BETA',
+              label: '21 R2',
               banner: 'none',
             },
               '21': {
